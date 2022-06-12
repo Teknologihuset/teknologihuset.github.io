59548fe3555e2a04257957aedda08c9c8f781d74
dervism <dervis4@gmail.com>
2022-06-12 17:19:01 +0200

    Update to React 18 syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import FrontPage from './pages/frontpage/FrontPage'
 import '../public/assets/sass/main.scss'
+import * as query from 'react-query'
+import {RecoilRoot} from "recoil";
 
-ReactDOM.render(
+//import dotenv from "dotenv"
+//dotenv.config()
+
+const queryClient = new query.QueryClient({
+    defaultOptions: {
+        queries: {
+            retryDelay: 500,
+            retry: 1,
+            refetchOnWindowFocus: false,
+            //cacheTime: 0,
+            cacheTime: 1000 * 60 * 60 // 1 hour, add * xx for more
+        },
+    },
+})
+
+const container = document.getElementById('root');
+// @ts-ignore
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
-    <FrontPage />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+      <RecoilRoot>
+          <query.QueryClientProvider client={queryClient}>
+              <FrontPage />
+          </query.QueryClientProvider>
+      </RecoilRoot>
+  </React.StrictMode>);
