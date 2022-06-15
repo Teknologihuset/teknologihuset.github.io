06f251fa9886ca9abf6a22f582dfba5a9f101571
dervism <dervis4@gmail.com>
2022-06-15 23:10:29 +0200

    Adding context handling (wip)

---
+++ src/core/state/context.ts
@@ -0,0 +1,13 @@
+import React, { useMemo } from 'react'
+
+//For å slippe defaultvalue til context.
+//https://kentcdodds.com/blog/how-to-use-react-context-effectively
+// @ts-ignore
+export const createContext = <T>() => React.createContext<T | undefined>(undefined)
+export const useContext = <T>(context: React.Context<T | undefined>) => {
+    const ret = React.useContext(context)
+    if (ret === undefined) {
+        throw Error('Outside context')
+    }
+    return ret
+}
